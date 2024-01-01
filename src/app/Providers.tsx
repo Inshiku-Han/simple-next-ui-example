fffeb798ba7aee35fd18522d18feea9d38109bd9
Inshiku-Han <inshiku.han@wise-vine.com>
2024-01-01 18:19:38 +0900

    feat: Implement nextui

---
+++ src/app/Providers.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { NextUIProvider } from '@nextui-org/react'
+import { useRouter } from 'next/navigation'
+import { type PropsWithChildren } from 'react'
+
+export function Providers({ children }: PropsWithChildren) {
+  const router = useRouter()
+  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+import { type Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/react'
+import plugin from 'tailwindcss/plugin'
 
-const config: Config = {
+export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{ts,tsx,mdx}',
+    './src/components/**/*.{ts,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-}
-export default config
+  darkMode: 'class',
+  plugins: [
+    nextui(),
+    plugin(({ addComponents }) => {
+      addComponents({
+        '.custom-container': {
+          '@apply mx-auto max-w-screen-lg px-6': {},
+        },
+      })
+    }),
+  ],
+} satisfies Config
